8f2e7d2dcba982045ea5cc2ffc6fba90776a6310
amukredam5-coder <amukredam5@gmail.com>
2026-03-11 00:53:57 +0700

    Update API GET and POST for Flutter

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,3 @@ use App\Http\Controllers\SparepartController;
 
 // 1. Ambil data (GET) - ini sudah ada di gambar kamu
 Route::get('/spareparts', [SparepartController::class, 'getApiData']);
-
-// 2. Tambah data (POST) - Tambahkan baris ini di bawahnya
-Route::post('/spareparts', [SparepartController::class, 'storeApiData']);
