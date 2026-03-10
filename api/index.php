8b3749f33a530fee0d7fd2daa4900c23f1f94596
amukredam5-coder <amukredam5@gmail.com>
2026-03-10 20:26:03 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
-require __DIR__ . '/../public/index.php';
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$db   = getenv('DB_DATABASE');
+$user = getenv('DB_USERNAME');
+$pass = getenv('DB_PASSWORD');
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    echo "<h1>Koneksi Aiven Berhasil!</h1>";
+    echo "Terhubung ke: " . $host;
+} catch (PDOException $e) {
+    echo "<h1>Koneksi Gagal:</h1> " . $e->getMessage();
+}
+?>
