46012731187a0182a767c3a63f25553fda91567b
amukredam5-coder <amukredam5@gmail.com>
2026-03-11 21:40:00 +0700

    Update rute api spareparts dan tes koneksi

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,10 @@ use App\Http\Controllers\SparepartController;
 
 // 1. Ambil data (GET) - ini sudah ada di gambar kamu
 Route::get('/spareparts', [SparepartController::class, 'getApiData']);
+Route::get('/tes-koneksi', function() {
+    return response()->json([
+        'status' => 'Berhasil',
+        'pesan' => 'API Heri Motor sudah Online!',
+        'data' => 'Siap digunakan untuk Flutter'
+    ]);
+});
