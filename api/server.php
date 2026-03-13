5bbf655c2601397e39af7db9b03ed1149a596bb3
amukredam5-coder <amukredam5@gmail.com>
2026-03-13 22:35:15 +0700

    Redirect: Domain utama langsung ke tampilan database

--- api/server.php
+++ api/server.php
@@ -1,1 +1,5 @@
-<?php require __DIR__ . '/../public/index.php';
+<?php
+// Memaksa Laravel menganggap permintaan datang ke /api/cek-database
+$_SERVER['REQUEST_URI'] = '/api/cek-database';
+
+require __DIR__ . '/../public/index.php';
